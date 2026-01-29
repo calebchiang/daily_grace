2f8cabdd4e38aec3bb37b994dec2f16f5cfc6c0c
Caleb Chiang <calebchiang1002@gmail.com>
2026-01-29 12:35:53 -0800

    Initial Commit

---
+++ bible_ios/bible_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  bible_iosApp.swift
+//  bible_ios
+//
+//  Created by Caleb Chiang on 2026-01-29.
+//
+
+import SwiftUI
+
+@main
+struct bible_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bible_iosTests/bible_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  bible_iosTests.swift
+//  bible_iosTests
+//
+//  Created by Caleb Chiang on 2026-01-29.
+//
+
+import Testing
+@testable import bible_ios
+
+struct bible_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
